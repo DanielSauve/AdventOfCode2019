fbde6019c8509f794b181f91c82d73c535138e7d
danielsauve <djsauve@rogers.com>
2019-12-18 18:53:40 -0500

    Solution for Day5 Part 2

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -13,8 +13,8 @@ def run(prog):
     curr = 0
     while prog[curr] != 99:
         instruction = prog[curr]
+        modes = calculate_modes(instruction)
         if instruction % 10 == 1:
-            modes = calculate_modes(instruction)
             val1 = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
             val2 = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
             out = prog[curr + 3]
@@ -24,7 +24,6 @@ def run(prog):
                 return
             curr += 4
         elif instruction % 10 == 2:
-            modes = calculate_modes(instruction)
             val1 = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
             val2 = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
             out = prog[curr + 3]
@@ -37,9 +36,32 @@ def run(prog):
             prog[prog[curr + 1]] = int(input("Please input a value: "))
             curr += 2
         elif instruction % 10 == 4:
-            modes = calculate_modes(instruction)
             print(prog[curr + 1] if modes[0] else prog[prog[curr + 1]])
             curr += 2
+        elif instruction % 10 == 5:
+            val = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
+            if val != 0:
+                curr = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
+            else:
+                curr += 3
+        elif instruction % 10 == 6:
+            val = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
+            if val == 0:
+                curr = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
+            else:
+                curr += 3
+        elif instruction % 10 == 7:
+            val1 = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
+            val2 = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
+            out = prog[curr + 3]
+            prog[out] = 1 if val1 < val2 else 0
+            curr += 4
+        elif instruction % 10 == 8:
+            val1 = prog[curr + 1] if modes[0] else prog[prog[curr + 1]]
+            val2 = prog[curr + 2] if modes[1] else prog[prog[curr + 2]]
+            out = prog[curr + 3]
+            prog[out] = 1 if val1 == val2 else 0
+            curr += 4
     return prog[0]
 
 
