6da54c83a516c97cd4021467dc931942043a0a8d
danielsauve <djsauve@rogers.com>
2019-12-25 12:51:19 -0500

    Solution for Day 16 Part 2

--- Day 16/day16.py
+++ Day 16/day16.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     f = open("day16.txt").read()
     curr_num = [int(x) for x in f]
     memo = dict()
-    memo[0] = [0, 1, 0, -1]
     for _ in range(100):
         next_num = []
         for i in range(len(curr_num)):
@@ -24,5 +23,17 @@ if __name__ == "__main__":
                 num += item * pattern[curr_pat]
                 curr_pat = (curr_pat + 1) % len(pattern)
             next_num.append(abs(num) % 10)
+        curr_num = next_num
+    print("".join(map(lambda x: str(x), curr_num[:8])))
+    curr_num = [int(x) for x in f * 10000]
+    offset = int("".join(map(lambda x: str(x), curr_num[:7])))
+    curr_num = curr_num[offset:]
+    for _ in range(100):
+        next_num = []
+        total = sum(curr_num)
+        for i in range(len(curr_num)):
+            if i > 0:
+                total -= curr_num[i - 1]
+            next_num.append(total % 10)
         curr_num = next_num
     print("".join(map(lambda x: str(x), curr_num[:8])))
